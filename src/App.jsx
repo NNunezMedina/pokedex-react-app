e3a531cce5c43f71bf521e077dedeb90a83ac000
Nathaly <nathalynunezmedina@gmail.com>
2024-08-03 17:21:14 -0500

    "ADD userState and conditional rendering Home"

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
+import Home from "./components/Home";
 import Loginform from "./components/Loginform";
 
 function App() {
 
+  const [user, setUser] = useState([]);
+
   return (
     <>
-      <Loginform>
-        
-      </Loginform>
+    {
+      user.length <= 0 ? <Loginform setUser={setUser} /> : <Home user={user}/>
+    }
+      
     </>
   );
 }

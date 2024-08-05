bdeb2661d33e7289eb8ab52f3e43342b0d5f24a4
Nathaly <nathalynunezmedina@gmail.com>
2024-08-04 21:40:53 -0500

    "SET errors of fetch"

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,15 @@
 import { Search } from "lucide-react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import pokemonTitle from "../assets/pokemonTitle.png"
 
 const Home = ({ user }) => {
   const [input, setInput] = useState("");
   const [pokemonData, setPokemonData] = useState("");
+  const [error, setError] = useState("");
 
   function handleChange(event) {
-    setInput(event.target.value.toLowerCase());
+    setInput(event.target.value);
   }
 
   const handleKeyDown = (event) => {
@@ -17,28 +19,43 @@ const Home = ({ user }) => {
   };
 
   const fetchPokemonData = (pokemonName) => {
+    setError("");
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}/`)
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) 
+          if(response.status == 404) {
+            setError("Page nout found")
+          } else {
+            setError(`Pokemon not found: ${pokemonName}`);
+            return;
+          }
+        return response.json()
+      })
       .then((data) => {
         setPokemonData(data);
+      })
+      .catch(() => {
+        setPokemonData("");
+        setError("Page not found");
       });
   };
 
-  const handleClick = () => {
-    fetchPokemonData(input.toLowerCase());
-  };
 
   return (
-    <div className="flex flex-col h-screen w-full max-w-md mx-auto p-4 sm:max-w-lg md:max-w-xl lg:max-w-2xl items-center">
-      <div className="flex gap-[15px]">
-        <h1 className="text-2xl font-bold text-center flex-grow">
+    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <div className=" mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <h1 className="text-2xl font-bold text-center">
           What Pokemon are you looking for {user}?
         </h1>
+        <div className="flex justify-center">
+        <img src={pokemonTitle} alt="Pokemon title" className="m-8" />
+        </div>
       </div>
-        <div className="flex items-center bg-gray-100 rounded-[10px] p-3 gap-2">
+      <div className="flex justify-center">
+        <div className="flex items-center bg-gray-100 rounded-[10px] p-3 gap-2 w-full max-w-md">
           <Search />
           <input
-            className="bg-gray-100 rounded-[10px] focus:outline-none "
+            className="bg-gray-100 rounded-[10px] focus:outline-none sm:text-sm sm:leading-6 "
             type="text"
             placeholder="Search Pokemon"
             value={input}
@@ -46,15 +63,10 @@ const Home = ({ user }) => {
             onKeyDown={handleKeyDown}
           />
         </div>
-        <button 
-          className="bg-gray-100 rounded-[10px] p-3 hover:bg-gray-300 ml-2 w-24 m-5" // Ajustar el ancho del botón
-          onClick={handleClick}
-        >
-          Search
-        </button>
-
+      </div>
       <div className="flex flex-col items-center mt-4">
-        {pokemonData ? (
+      {error && <p className="text-red-600">{error}</p>}
+      {pokemonData ? (
           <>
             <h2 className="text-lg font-semibold">{pokemonData.name}</h2>
             <img
@@ -63,16 +75,17 @@ const Home = ({ user }) => {
               className="w-32 h-32 md:w-48 md:h-48"
             />
           </>
-        ) : (
-          <p>No pokemon found</p>
-        )}
+        ) : !error && <p></p>}
       </div>
+      <div className="flex justify-center m-2 items-center">
       <Link
         to="/pokedex-react-app/"
-        className="flex justify-center items-center bg-gray-100 rounded-[10px] hover:bg-gray-300 ml-2 p-2 m-5"
+        className=" p-[10px] font-bold text-violet-600"
       >
         Close session
       </Link>
+
+      </div>
     </div>
   );
 };

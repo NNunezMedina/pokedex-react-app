2f2a6da3a9132a49f39e3549dc1028413e93fe9d
Nathaly <nathalynunezmedina@gmail.com>
2024-08-05 21:58:23 -0500

    "ADD fetch and USE AuthContext"

---
+++ src/cofig.js
@@ -0,0 +1,3 @@
+export const tokenKey = "poke-collection-token";
+export const BASE_URI = "https://poke-collection-api-production.up.railway.app";
+
--- src/components/Loginform.jsx
+++ src/components/Loginform.jsx
@@ -2,14 +2,17 @@ import { useState } from "react";
 import Input from "./Input";
 import { Link, useNavigate } from "react-router-dom";
 import pokemonLogo from "../assets/pokemon_logo.png";
+import { useAuth } from "../context/Auth-Context";
 
 const Loginform = ({setUser}) => {
+  const {login} = useAuth(); // Obtener la función de login del contexto
   const [loginData, setLoginData] = useState({
     email: "",
     password: "",
   });
 
-  const [errorLogin, setErrorLogin] = useState(false)
+  const [errorLogin, setErrorLogin] = useState(false);
+  const [apiError, setApiError] = useState("");
   const navigate = useNavigate();
 
   function handleSubmit(event) {
@@ -19,14 +22,40 @@ const Loginform = ({setUser}) => {
       return;
     }
     setErrorLogin(false);
-    setUser([loginData.email]);
-    navigate("/pokedex-react-app/home");
+
+    const options = {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        email:loginData.email,
+        password:loginData.password
+      })
+    };
+
+    fetch('https://poke-collection-api-production.up.railway.app//login', options)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+  .then(data => {
+    if(data.token) {
+      login({email: loginData.email}, data.token)
+      navigate("/pokedex-react-app/home");
+    }
+  })
+  .catch(err => {
+    console.error('Fetch error:',err);
+    setApiError("User doesn't exist. Please create an account")
+  });
   }
 
   function handleChange(event) {
     const { name, value } = event.target;
     setLoginData({ ...loginData, [name]: value });
-    setErrorLogin("");
+    setErrorLogin(false);
+    setApiError("");
   }
 
   return (
@@ -63,6 +92,7 @@ const Loginform = ({setUser}) => {
           </div>
         </form>
         {errorLogin && <p className="flex justify-center text-red-600">All fields are required</p>}
+        {apiError && <p className="flex justify-center text-red-600 mt-2">{apiError}</p>}
         <div className="flex justify-center m-2 items-center">
             Dont have an account?
               <Link to="/pokedex-react-app/create-account" className=" p-[10px] font-bold text-violet-600">Create Account</Link>

b1a9aa8b777ebbe1c916454204a7b0210142fe02
Nathaly <nathalynunezmedina@gmail.com>
2024-10-01 19:28:09 -0500

    "ADD api-fetch logic por API"

---
+++ src/services/api-fetch.js
@@ -0,0 +1,50 @@
+import { BASE_URI } from "../cofig";
+
+export const loginUser = async (email, password) => {
+  const options = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, password }),
+  };
+
+  const response = await fetch(`${BASE_URI}/login`, options);
+
+  if (!response.ok) {
+    const errData = await response.json();
+    console.error("Error details:", errData);
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  return response.json();
+};
+
+export const fetchUserProfile = async (token) => {
+  const options = {
+    method: "GET",
+    headers: { Authorization: `Bearer ${token}` },
+  };
+  const response = await fetch(`${BASE_URI}/profile`, options);
+  
+  if (!response.ok) {
+    throw new Error("Failed to fetch user profile");
+  }
+  
+  return response.json();
+};
+
+export const createUser = async (userData) => {
+  const options = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(userData),
+  };
+
+  const response = await fetch(`${BASE_URI}/signup`, options);
+
+  if (!response.ok) {
+    const errData = await response.json();
+    throw new Error(errData.message || "Failed to create user");
+  }
+
+  return response.json();
+};

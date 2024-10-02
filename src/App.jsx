5a1c21c43d35f488d6a8963b448107455ed80e54
Nathaly <nathalynunezmedina@gmail.com>
2024-10-02 18:00:50 -0500

    "UPDATE route simplicity"

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { AuthProvider, useAuth } from "./context/Auth-Context";
 import Authenticated from "./components/Authenticated";
 import Unauthenticated from "./components/Unauthenticated";
-import CreateAccountForm from "./components/CreateAccountForm";
 
 function App() {
   return (
@@ -15,27 +14,29 @@ function App() {
 const MainRoutes = () => {
   const { user } = useAuth();
 
-  console.log("Current user:", user); // Para verificar el estado del usuario
-
   return (
     <Routes>
       <Route
-        path="/pokedex-react-app/"
+        path="/pokedex-react-app/*"
         element={
-          user ? <Navigate to="/pokedex-react-app/home" replace /> : <Unauthenticated />
+          user ? (
+            <Navigate to="/pokedex-react-app/home" replace />
+          ) : (
+            <Unauthenticated />
+          )
         }
       />
+
       <Route
         path="/pokedex-react-app/home/*"
-        element={user ? <Authenticated user={user} /> : <Navigate to="/pokedex-react-app/" replace />}
-      />
-      <Route
-        path="/pokedex-react-app/create-account/*" // Cambiar a "/*"
         element={
-          user ? <Navigate to="/pokedex-react-app/home" replace /> : <Unauthenticated />
+          user ? (
+            <Authenticated user={user} />
+          ) : (
+            <Navigate to="/pokedex-react-app" replace />
+          )
         }
       />
-      <Route path="/pokedex-react-app/create-account" element={<CreateAccountForm />} />
     </Routes>
   );
 };

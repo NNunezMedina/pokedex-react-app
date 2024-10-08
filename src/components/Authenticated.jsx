b56faf07e266b010ab8be9adb6c18e7ad0eb60cd
Nathaly <nathalynunezmedina@gmail.com>
2024-10-08 13:46:18 -0500

    "ADD react lazy suspense for loading Home"

--- src/components/Authenticated.jsx
+++ src/components/Authenticated.jsx
@@ -1,14 +1,31 @@
 import { Routes, Route } from "react-router-dom";
 import PropTypes from "prop-types"; 
-import Home from "./Home";
+import { Suspense, lazy } from "react";
 import Pokedex from "./Pokedex";
 import Favorites from "./Favorites/PokemonFavorites";
 import ZodiacGame from "./Minigame/ZodiacGame";
+import Lottie from "lottie-react";
+import LoadingSpinner from "../assets/LoadingSpinner.json"
+
+const Home = lazy(() => import("./Home"));
 
 const Authenticated = ({ user }) => {
   return (
     <Routes>
-      <Route path="/" element={<Home user={user} />} /> 
+      <Route
+        path="/"
+        element={
+          <Suspense
+            fallback={
+              <div className="flex items-center justify-center h-screen">
+                <Lottie animationData={LoadingSpinner} loop style={{ height: 150, width: 150 }} />
+              </div>
+            }
+          >
+            <Home user={user} />
+          </Suspense>
+        }
+      />
       <Route path="/pokedex" element={<Pokedex />} /> 
       <Route path="/favorites" element={<Favorites />} />
       <Route path = "/minigame" element={<ZodiacGame/>}/>
